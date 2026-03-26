36788be80a7784c0c9652b9952e2fb707bec14d5
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-25 21:04:10 -0300

    Add the logic so that the characters are treated as not just a and not just numbers, and see how I can do it for local storage and improve the CSS and the &copy;

---
+++ java.js
@@ -0,0 +1,22 @@
+let inputnome = document.getElementById("inputnome")
+let inputsenha  = document.getElementById("inputsenha")
+let enviar = document.getElementById("enviar")
+
+enviar.addEventListener("click",(e)=>{
+
+    // e.preventDefault();
+
+    if (inputnome.value === "") {
+       return alert("adicione o nome")
+    }
+    else if (inputsenha.value==="") {
+       return alert("adicione a senha")
+    }
+
+    if (inputnome) {
+        
+        
+
+    }
+
+})
